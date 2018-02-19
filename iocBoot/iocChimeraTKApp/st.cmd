832903871029d411795f210d7cf990fb69850db9
Martin Hierholzer <martin.hierholzer@desy.de>
2018-02-19 15:38:56 +0100

    install a template directory structure for an IOC

---
+++ iocBoot/iocChimeraTKApp/st.cmd
@@ -0,0 +1,17 @@
+#!<ApplicationExecutable>
+
+## Register all support components
+dbLoadDatabase("../../dbd/ChimeraTK.dbd",0,0)
+ChimeraTK_registerRecordDeviceDriver(pdbbase)
+
+## Setup ChimeraTKApp application and set polling intervall to 100us
+chimeraTKConfigureApplication("ChimeraTKApp", 100)
+
+## Load record instances
+dbLoadRecords("../../db/<ApplicationName>.db","P=<InstanceName>,APP=<ApplicationName>")
+
+## Initialise the IOC
+iocInit()
+
+## Start any sequence programs
+#seq sncllrfctrl,"user=<user>"
